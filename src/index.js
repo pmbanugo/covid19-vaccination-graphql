d7632358ea5f5b63649eff4f7b2df7a5e21ec326
Peter Mbanugo <chuksgerrard@yahoo.com>
2021-02-23 15:22:32 +0100

    add NewRelic Node.js agent & apollo server integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import newrelic from "newrelic";
+import plugin from "@newrelic/apollo-server-plugin";
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./api/typeDefs.js";
 import resolvers from "./api/resolvers.js";
@@ -5,6 +8,7 @@ import resolvers from "./api/resolvers.js";
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [plugin],
   introspection: true,
   playground: true,
 });
